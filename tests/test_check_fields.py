702fcd23aaa259b424f40779c0181b0f5d021690
Germain Sauve <germain.sauve@gmail.com>
2022-05-11 16:40:01 -0400

    Implemented check fields tests for the 4 types

--- tests/test_check_fields.py
+++ tests/test_check_fields.py
@@ -2,20 +2,17 @@
 import pandas as pd
 from pyobistools.validation import check_fields
 
-field_data = pd.DataFrame({
-  'occurrenceID': ["1", "2", "3"],
-  'scientificName': ["Abra alba", pd.NA, ""],
-  'locality': ["North Sea", "English Channel", "Flemish Banks"],
-  'minimumDepthInMeters': ["10", "", "5"]})
-
-# require more test date for other analysis type
-
 
 def test_check_fields_default():
     """
     check_fields_default detects missing or empty required and recommended fields
-    for the occurrence_core fromat. This is the default behaviour for check_fields
+    for the occurrence_core format. This is the default behaviour for check_fields
     """
+    field_data = pd.DataFrame({
+      'occurrenceID': ["1", "2", "3"],
+      'scientificName': ["Abra alba", pd.NA, ""],
+      'locality': ["North Sea", "English Channel", "Flemish Banks"],
+      'minimumDepthInMeters': ["10", "", "5"]})
     # required terms
     error = check_fields.check_fields(field_data)
     # print(error)
@@ -23,24 +20,28 @@ def test_check_fields_default():
 
     # recommended terms
     error = check_fields.check_fields(field_data, level="warning")
-    print(error)
     assert len(error.index) == 10
 
 
 def test_check_fields_eventcore():
     """
-    check_fields_default detects missing or empty required and recommended fields
-    for the occurrence_core fromat. This is the default behaviour for check_fields
+    check_fields_eventcore detects missing or empty required and recommended fields
+    for the event_core format.
+
     """
+    field_data = pd.DataFrame({
+      'eventid': ["1", "2", "3"],
+      'decimallatitude': [46.0, 48, 31.43563],
+      'countrycode': ["CA", "CA", "US"],
+      })
+
     # required terms
     error = check_fields.check_fields(field_data, analysis_type="event_core")
-    # print(error)
-    assert len(error.index) == 9
+    assert len(error.index) == 3
 
     # recommended terms
     error = check_fields.check_fields(field_data, analysis_type="event_core", level="warning")
-    print(error)
-    assert len(error.index) == 10
+    assert error is None
 
 
 def test_check_fields_occurrence_extension():
@@ -48,15 +49,20 @@ def test_check_fields_occurrence_extension():
     check_fields_default detects missing or empty required and recommended fields
     for the occurrence_core fromat. This is the default behaviour for check_fields
     """
+    field_data = pd.DataFrame({
+      'eventid': ["1", "1", "2"],
+      'occurrenceID': ["1", "2", "3"],
+      'scientificName': ["Abra alba", pd.NA, ""],
+      'occurrencestatus': ["North Sea", "English Channel", "Flemish Banks"]
+      })
     # required terms
     error = check_fields.check_fields(field_data, analysis_type="occurrence_extension")
     print(error)
-    assert len(error.index) == 9
+    assert len(error.index) == 3
 
     # recommended terms
     error = check_fields.check_fields(field_data, analysis_type="occurrence_extension", level="warning")
-    print(error)
-    assert len(error.index) == 10
+    assert error is None
 
 
 def test_check_fields_extended_measurement_or_fact():
@@ -64,12 +70,20 @@ def test_check_fields_extended_measurement_or_fact():
     check_fields_default detects missing or empty required and recommended fields
     for the occurrence_core fromat. This is the default behaviour for check_fields
     """
+    field_data = pd.DataFrame({
+      'eventid': ["1", "1", "1"],
+      'occurrenceID': ["1", "1", "2"],
+      'measurementid': ["1", "2", "3"],
+      'measurementtype': ["temperature", "tail length", "temperature"],
+      'measurementvalue': [12, 12.7, 15.873],
+      'measurementunit': ["C", "cm", "C"],
+      'measurementremarks': ["", pd.NA, "not calibrated"]
+      })
     # required terms
-    error = check_fields.check_fields(field_data, analysis_type="extended_measurement_or_fact")
+    error = check_fields.check_fields(field_data, analysis_type="extended_measurement_or_fact_extension")
     print(error)
-    assert len(error.index) == 9
+    assert len(error.index) == 7
 
     # recommended terms
-    error = check_fields.check_fields(field_data, analysis_type="extended_measurement_or_fact", level="warning")
-    print(error)
-    assert len(error.index) == 10
+    error = check_fields.check_fields(field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
+    assert error is None
