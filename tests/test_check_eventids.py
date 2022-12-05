eaeedc5c1986f944c6c4bac215f75010d4f471af
Ryan Gosse <ryan.gosse@dal.ca>
2022-12-05 15:08:52 -0400

    Fixed check fields tests to check for empty dataframe instead of None

--- tests/test_check_eventids.py
+++ tests/test_check_eventids.py
@@ -2,31 +2,36 @@
 check eventIDs
 """
 
-import pandas as pd
-from pyobistools.validation.check_eventids import check_eventids, check_extension_eventids
 import numpy as np
+import pandas as pd
+
+from pyobistools.validation.check_eventids import (check_eventids,
+                                                   check_extension_eventids)
+
 NaN = np.nan
 
+
 def test_check_eventids_presence_eventids_parenteventid():
-  field_data = pd.DataFrame({
-      'occurrenceID': ["1", "2", "3"],
-      'measurementid': ["1", "2", "3"],
-      'measurementtype': ["temperature", "tail length", "temperature"],
-      'measurementvalue': [12, 12.7, 15.873],
-      'measurementunit': ["C", "cm", "C"],
-      'measurementremarks': ["", NaN, "not calibrated"]
-      })
-  correct_data = pd.DataFrame(data={
-    'field': ["eventid", "parenteventid"], 
-    'level': ["error", "error"], 
-    'row' :  ['NaN','NaN'],
-    'message': ["Field eventid is missing", "Field parenteventid is missing"] })
-  
-  error = check_eventids(field_data)
+    field_data = pd.DataFrame({
+        'occurrenceID': ["1", "2", "3"],
+        'measurementid': ["1", "2", "3"],
+        'measurementtype': ["temperature", "tail length", "temperature"],
+        'measurementvalue': [12, 12.7, 15.873],
+        'measurementunit': ["C", "cm", "C"],
+        'measurementremarks': ["", NaN, "not calibrated"]
+    })
+    correct_data = pd.DataFrame(data={
+        'field': ["eventid", "parenteventid"],
+        'level': ["error", "error"],
+        'row':  ['NaN', 'NaN'],
+        'message': ["Field eventid is missing", "Field parenteventid is missing"]})
+
+    error = check_eventids(field_data)
 
     # reset index of both dataframe or the compare won't work
     # assert correct_data.reset_index(drop=True).equals(error.reset_index(drop=True)) == True
-  assert correct_data.astype(str).reset_index(drop=True).equals(error.astype(str).reset_index(drop=True)) == True
+    assert correct_data.astype(str).reset_index(drop=True).equals(
+        error.astype(str).reset_index(drop=True)) == True
 
 
 def test_check_eventids_duplicate_eventIDs():
@@ -36,19 +41,18 @@ def test_check_eventids_duplicate_eventIDs():
 
     """
     field_data = pd.DataFrame({
-      'eventid': [1, 1, 3],
-      'decimallatitude': [46.0, 48, ''],
-      'parenteventid': [1, 1, 1],
-      'countrycode': ["CA", "CA", "US"],
-      })
+        'eventid': [1, 1, 3],
+        'decimallatitude': [46.0, 48, ''],
+        'parenteventid': [1, 1, 1],
+        'countrycode': ["CA", "CA", "US"],
+    })
 
     error = check_eventids(field_data)
     assert len(error.index) == 2
 
 
-
 def test_check_eventids_parenteventids_corresponding_eventIDs():
-  field_data = pd.DataFrame({
+    field_data = pd.DataFrame({
         'eventid': ["1", "2", "3"],
         'parenteventid': ["1", "2", "4"],
         'measurementid': ["1", "2", "3"],
@@ -56,24 +60,23 @@ def test_check_eventids_parenteventids_corresponding_eventIDs():
         'measurementvalue': [12, 12.7, 15.873],
         'measurementunit': ["C", "cm", "C"],
         'measurementremarks': ["", NaN, "not calibrated"]
-        })
+    })
 
-  correct_data = pd.DataFrame(data={
-      'field': ["parenteventid"], 
-      'level': ["error"], 
-      'row' :  [2],
-      'message': ["parenteventid 4 has no corresponding eventID"] })
+    correct_data = pd.DataFrame(data={
+        'field': ["parenteventid"],
+        'level': ["error"],
+        'row':  [2],
+        'message': ["parenteventid 4 has no corresponding eventID"]})
 
+    error = check_eventids(field_data)
 
-  error = check_eventids(field_data)
-
-  assert correct_data.astype(str).reset_index(drop=True).equals(error.astype(str).reset_index(drop=True)) == True
-
+    assert correct_data.astype(str).reset_index(drop=True).equals(
+        error.astype(str).reset_index(drop=True)) == True
 
 
 def test_check_eventids_eventids_extension_corresponding_core():
 
-  event = pd.DataFrame({
+    event = pd.DataFrame({
         'eventid': ["1", "2", "3"],
         'parenteventid': ["1", "2", "4"],
         'measurementid': ["1", "2", "3"],
@@ -81,9 +84,9 @@ def test_check_eventids_eventids_extension_corresponding_core():
         'measurementvalue': [12, 12.7, 15.873],
         'measurementunit': ["C", "cm", "C"],
         'measurementremarks': ["", NaN, "not calibrated"]
-        })
+    })
 
-  extension = pd.DataFrame({
+    extension = pd.DataFrame({
         'eventid': ["1", "6", "3"],
         'parenteventid': ["1", "2", "4"],
         'measurementid': ["1", "2", "3"],
@@ -91,7 +94,7 @@ def test_check_eventids_eventids_extension_corresponding_core():
         'measurementvalue': [12, 12.7, 15.873],
         'measurementunit': ["C", "cm", "C"],
         'measurementremarks': ["", NaN, "not calibrated"]
-        })
+    })
 
-  error = check_extension_eventids(event=event, extension=extension)
-  assert len(error.index) == 1
+    error = check_extension_eventids(event=event, extension=extension)
+    assert len(error.index) == 1
--- tests/test_check_fields.py
+++ tests/test_check_fields.py
@@ -1,8 +1,11 @@
 
-import pandas as pd
-from pyobistools.validation.check_fields import check_fields
 import numpy as np
+import pandas as pd
+
+from pyobistools.validation.check_fields import check_fields
+
 NaN = np.nan
+
 
 def test_check_fields_default():
     """
@@ -10,21 +13,22 @@ def test_check_fields_default():
     for the occurrence_core format. This is the default behaviour for check_fields
     """
     field_data = pd.DataFrame({
-      'occurrenceID': ["1", "2", "3"],
-      'scientificName': ["Abra alba", NaN, ""],
-      'locality': ["North Sea", "English Channel", "Flemish Banks"],
-      'minimumDepthInMeters': ["10", "", "5"]})
+        'occurrenceID': ["1", "2", "3"],
+        'scientificName': ["Abra alba", NaN, ""],
+        'locality': ["North Sea", "English Channel", "Flemish Banks"],
+        'minimumDepthInMeters': ["10", "", "5"]})
     # required terms
     correct_data = pd.DataFrame(data={
-        'field': ["basisofrecord", "scientificnameid", "eventdate", "decimallatitude", "decimallongitude", "occurrencestatus", "countrycode", "kingdom", "geodeticdatum", "scientificname", "scientificname"], 
-        'level': ["error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"], 
+        'field': ["basisofrecord", "scientificnameid", "eventdate", "decimallatitude", "decimallongitude", "occurrencestatus", "countrycode", "kingdom", "geodeticdatum", "scientificname", "scientificname"],
+        'level': ["error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"],
         'row': ['NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', '1', '2'],
         'message': ["Required field basisofrecord is missing", "Required field scientificnameid is missing", "Required field eventdate is missing", "Required field decimallatitude is missing", "Required field decimallongitude is missing", "Required field occurrencestatus is missing", "Required field countrycode is missing", "Required field kingdom is missing", "Required field geodeticdatum is missing", 'Empty value for required field scientificname', 'Empty value for required field scientificname']
-        })
+    })
     error = check_fields(field_data)
 
     # reset index of both dataframe or the compare won't work
-    assert correct_data.astype(str).reset_index(drop=True).equals(error.astype(str).reset_index(drop=True)) == True
+    assert correct_data.astype(str).reset_index(drop=True).equals(
+        error.astype(str).reset_index(drop=True)) == True
 
     # recommended terms
     error = check_fields(field_data, level="warning")
@@ -38,10 +42,10 @@ def test_check_fields_eventcore():
 
     """
     field_data = pd.DataFrame({
-      'eventid': ["1", "2", "3"],
-      'decimallatitude': [46.0, 48, ''],
-      'countrycode': ["CA", "CA", "US"],
-      })
+        'eventid': ["1", "2", "3"],
+        'decimallatitude': [46.0, 48, ''],
+        'countrycode': ["CA", "CA", "US"],
+    })
 
     # required terms
     error = check_fields(field_data, analysis_type="event_core")
@@ -49,7 +53,8 @@ def test_check_fields_eventcore():
 
     # recommended terms
     error = check_fields(field_data, analysis_type="event_core", level="warning")
-    assert error is None
+    print("affert: ", error)
+    assert error.empty
 
 
 def test_check_fields_occurrence_extension():
@@ -58,18 +63,18 @@ def test_check_fields_occurrence_extension():
     for the occurrence_core fromat. This is the default behaviour for check_fields
     """
     field_data = pd.DataFrame({
-      'eventid': ["1", "1", "2"],
-      'occurrenceID': ["1", "2", "3"],
-      'scientificName': ["Abra alba", NaN, ""],
-      'occurrencestatus': ["North Sea", "English Channel", "Flemish Banks"]
-      })
+        'eventid': ["1", "1", "2"],
+        'occurrenceID': ["1", "2", "3"],
+        'scientificName': ["Abra alba", NaN, ""],
+        'occurrencestatus': ["North Sea", "English Channel", "Flemish Banks"]
+    })
     # required terms
     error = check_fields(field_data, analysis_type="occurrence_extension")
     assert len(error.index) == 5
 
     # recommended terms
     error = check_fields(field_data, analysis_type="occurrence_extension", level="warning")
-    assert error is None
+    assert error.empty
 
 
 def test_check_fields_extended_measurement_or_fact():
@@ -78,18 +83,19 @@ def test_check_fields_extended_measurement_or_fact():
     for the occurrence_core fromat. This is the default behaviour for check_fields
     """
     field_data = pd.DataFrame({
-      'eventid': ["1", "1", "1"],
-      'occurrenceID': ["1", "1", "2"],
-      'measurementid': ["1", "2", "3"],
-      'measurementtype': ["temperature", "tail length", "temperature"],
-      'measurementvalue': [12, 12.7, 15.873],
-      'measurementunit': ["C", "cm", "C"],
-      'measurementremarks': ["", NaN, "not calibrated"]
-      })
+        'eventid': ["1", "1", "1"],
+        'occurrenceID': ["1", "1", "2"],
+        'measurementid': ["1", "2", "3"],
+        'measurementtype': ["temperature", "tail length", "temperature"],
+        'measurementvalue': [12, 12.7, 15.873],
+        'measurementunit': ["C", "cm", "C"],
+        'measurementremarks': ["", NaN, "not calibrated"]
+    })
     # required terms
     error = check_fields(field_data, analysis_type="extended_measurement_or_fact_extension")
     assert len(error.index) == 9
 
     # recommended terms
-    error = check_fields(field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
-    assert error is None
+    error = check_fields(
+        field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
+    assert error.empty
