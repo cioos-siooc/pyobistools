4d1c33fdf463435f6fb4ac7449fa1f200fd666bc
Simon Beauvillier <beauvilliers@ogsl.ca>
2023-03-10 09:14:09 -0500

    update test_check_fields

--- tests/test_check_fields.py
+++ tests/test_check_fields.py
@@ -9,8 +9,8 @@ NaN = np.nan
 
 def test_check_fields_default():
     """
-    check_fields_default detects missing or empty required and recommended fields
-    for the occurrence_core format. This is the default behaviour for check_fields
+    test_check_fields_default detects missing or empty required and recommended fields (if any)
+    for the occurrence_core format. It also checks for empty values and case sensitivity.
     """
     field_data = pd.DataFrame({
         'occurrenceID': ["1", "2", "3"],
@@ -19,48 +19,48 @@ def test_check_fields_default():
         'minimumDepthInMeters': ["10", "", "5"]})
     # required terms
     correct_data = pd.DataFrame(data={
-        'field': ["basisofrecord", "scientificnameid", "eventdate", "decimallatitude", "decimallongitude", "occurrencestatus", "countrycode", "kingdom", "geodeticdatum", "scientificname", "scientificname"],
+        'field': ["basisOfRecord", "scientificNameID", "eventDate", "decimalLatitude", "decimalLongitude", "occurrenceStatus", "countryCode", "kingdom", "geodeticDatum", "scientificName", "scientificName"],
         'level': ["error", "error", "error", "error", "error", "error", "error", "error", "error", "error", "error"],
         'row': ['NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', '1', '2'],
-        'message': ["Required field basisofrecord is missing", "Required field scientificnameid is missing", "Required field eventdate is missing", "Required field decimallatitude is missing", "Required field decimallongitude is missing", "Required field occurrencestatus is missing", "Required field countrycode is missing", "Required field kingdom is missing", "Required field geodeticdatum is missing", 'Empty value for required field scientificname', 'Empty value for required field scientificname']
+        'message': ["Required field basisOfRecord is missing", "Required field scientificNameID is missing", "Required field eventDate is missing", "Required field decimalLatitude is missing", "Required field decimalLongitude is missing", "Required field occurrenceStatus is missing", "Required field countryCode is missing", "Required field kingdom is missing", "Required field geodeticDatum is missing", 'Empty value for required field scientificName', 'Empty value for required field scientificName']
     })
-    error = check_fields(field_data)
 
-    # reset index of both dataframe or the compare won't work
+    # required terms & empty values
+    error = check_fields(field_data)
+    
     assert correct_data.astype(str).reset_index(drop=True).equals(
         error.astype(str).reset_index(drop=True))
 
-    # recommended terms
+    # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, level="warning")
     assert len(error.index) == 11
 
 
-def test_check_fields_eventcore():
+def test_check_fields_event_core():
     """
-    check_fields_eventcore detects missing or empty required and recommended fields
-    for the event_core format.
+    test_check_fields_event_core detects missing or empty required and recommended fields (if any)
+    for the event_core format. It also checks for empty values and case sensitivity.
 
     """
     field_data = pd.DataFrame({
-        'eventid': ["1", "2", "3"],
-        'decimallatitude': [46.0, 48, ''],
-        'countrycode': ["CA", "CA", "US"],
+        'eventID': ["1", "2", "3"],
+        'decimalLatitude': [46.0, 48, ''],
+        'countryCode': ["CA", "CA", "US"],
     })
 
-    # required terms
+    # required terms & empty values
     error = check_fields(field_data, analysis_type="event_core")
     assert len(error.index) == 4
 
-    # recommended terms
+    # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, analysis_type="event_core", level="warning")
-    print("affert: ", error)
     assert error.empty
 
 
 def test_check_fields_occurrence_extension():
     """
-    check_fields_default detects missing or empty required and recommended fields
-    for the occurrence_core fromat. This is the default behaviour for check_fields
+    test_check_fields_occurrence_extension detects missing or empty required and recommended fields (if any)
+    for the occurrence_extension fromat. It also checks for empty values and case sensitivity.
     """
     field_data = pd.DataFrame({
         'eventid': ["1", "1", "2"],
@@ -68,34 +68,35 @@ def test_check_fields_occurrence_extension():
         'scientificName': ["Abra alba", NaN, ""],
         'occurrencestatus': ["North Sea", "English Channel", "Flemish Banks"]
     })
-    # required terms
+
+    # required terms & empty values
     error = check_fields(field_data, analysis_type="occurrence_extension")
     assert len(error.index) == 5
 
-    # recommended terms
+    # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, analysis_type="occurrence_extension", level="warning")
-    assert error.empty
+    assert len(error.index) == 2
 
 
-def test_check_fields_extended_measurement_or_fact():
+def test_check_fields_extended_measurement_or_fact_extension():
     """
-    check_fields_default detects missing or empty required and recommended fields
-    for the occurrence_core fromat. This is the default behaviour for check_fields
+    test_check_fields_extended_measurement_or_fact_extension detects missing or empty required and recommended fields (if any)
+    for the extended_measurement_or_fact_extension fromat. It also checks for empty values and case sensitivity.
     """
     field_data = pd.DataFrame({
-        'eventid': ["1", "1", "1"],
+        'eventID': ["1", "1", "1"],
         'occurrenceID': ["1", "1", "2"],
-        'measurementid': ["1", "2", "3"],
-        'measurementtype': ["temperature", "tail length", "temperature"],
-        'measurementvalue': [12, 12.7, 15.873],
-        'measurementunit': ["C", "cm", "C"],
+        'measurementID': ["1", "2", "3"],
+        'measurementType': ["temperature", "tail length", "temperature"],
+        'measurementValue': [12, 12.7, 15.873],
+        'measurementUnit': ["C", "cm", "C"],
         'measurementremarks': ["", NaN, "not calibrated"]
     })
-    # required terms
+
+    # required terms & empty values
     error = check_fields(field_data, analysis_type="extended_measurement_or_fact_extension")
     assert len(error.index) == 9
 
-    # recommended terms
-    error = check_fields(
-        field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
-    assert error.empty
+    # recommended terms if any, empty values & case sensitivity check
+    error = check_fields(field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
+    assert len(error.index) == 1
