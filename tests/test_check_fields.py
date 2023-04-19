fdb46e0ba8b87a858c4dfa9b96cb72ca8700a020
Simon Beauvillier <beauvilliers@ogsl.ca>
2023-04-19 09:11:31 -0400

    modifications test_check_fields

--- tests/test_check_fields.py
+++ tests/test_check_fields.py
@@ -33,6 +33,8 @@ def test_check_fields_default():
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, level="warning")
+    error = pd.DataFrame(data=error)
+
     assert len(error.shape[0]) == 11
 
 
@@ -54,6 +56,8 @@ def test_check_fields_event_core():
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, analysis_type="event_core", level="warning")
+    error = pd.DataFrame(data=error)
+
     assert error.empty
 
 
@@ -71,10 +75,14 @@ def test_check_fields_occurrence_extension():
 
     # required terms & empty values
     error = check_fields(field_data, analysis_type="occurrence_extension")
+    error = pd.DataFrame(data=error)
+
     assert len(error.shape[0]) == 5
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, analysis_type="occurrence_extension", level="warning")
+    error = pd.DataFrame(data=error)
+
     assert len(error.shape[0]) == 2
 
 
@@ -95,9 +103,13 @@ def test_check_fields_extended_measurement_or_fact_extension():
 
     # required terms & empty values
     error = check_fields(field_data, analysis_type="extended_measurement_or_fact_extension")
+    error = pd.DataFrame(data=error)
+
     assert len(error.shape[0]) == 9
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(
         field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
+    error = pd.DataFrame(data=error)
+
     assert len(error.shape[0]) == 1
