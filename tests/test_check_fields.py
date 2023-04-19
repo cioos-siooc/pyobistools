57b318d0bd89f71f7060e7f355c5aba96846055a
Simon Beauvillier <beauvilliers@ogsl.ca>
2023-04-19 13:15:10 -0400

    correction check_fields, change test_check_fields

--- tests/test_check_fields.py
+++ tests/test_check_fields.py
@@ -33,7 +33,6 @@ def test_check_fields_default():
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, level="warning")
-    error = pd.DataFrame(data=error)
 
     assert error.shape[0] == 11
 
@@ -56,7 +55,6 @@ def test_check_fields_event_core():
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, analysis_type="event_core", level="warning")
-    error = pd.DataFrame(data=error)
 
     assert error.empty
 
@@ -75,13 +73,11 @@ def test_check_fields_occurrence_extension():
 
     # required terms & empty values
     error = check_fields(field_data, analysis_type="occurrence_extension")
-    error = pd.DataFrame(data=error)
 
     assert error.shape[0] == 5
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(field_data, analysis_type="occurrence_extension", level="warning")
-    error = pd.DataFrame(data=error)
 
     assert error.shape[0] == 2
 
@@ -103,13 +99,11 @@ def test_check_fields_extended_measurement_or_fact_extension():
 
     # required terms & empty values
     error = check_fields(field_data, analysis_type="extended_measurement_or_fact_extension")
-    error = pd.DataFrame(data=error)
 
     assert error.shape[0] == 9
 
     # recommended terms if any, empty values & case sensitivity check
     error = check_fields(
         field_data, analysis_type="extended_measurement_or_fact_extension", level="warning")
-    error = pd.DataFrame(data=error)
 
     assert error.shape[0] == 1
