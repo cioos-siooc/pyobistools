135a5abe98f24eb111f5e323cc3700cdc2cddfb0
Jon Pye <jdpye@dal.ca>
2023-11-24 14:28:32 -0400

    flake8 fixes

--- pyobistools/validation/check_onland.py
+++ pyobistools/validation/check_onland.py
@@ -74,8 +74,8 @@ def check_onland(data, land=None, report=False, buffer=0, offline=False):
         if len(gdf) > 0:
             return gdf[
                 gdf["on_land"]
-            ] # TODO: return a report format describing which rows are land-bound
-        else: # the function returns an empty dataframe
+            ]
+        else:   # the function returns an empty dataframe
             return pd.DataFrame(index=data.index, columns=data.columns)  # return empty dataframe in the same shape, as per the R implementation
     else:  # if we are not returning a report, return the offending rows themselves.
         return gdf[gdf["on_land"]]
